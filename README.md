a087f10e7ef9f1c05f8415c6e26a367a5cdd1c7d
Yuxuan Zhang <yzhangms@connect.ust.hk>
2023-09-18 16:54:52 +0800

    Modify README

--- README.md
+++ README.md
@@ -1,35 +1,34 @@
-# Watchlist
+# Data Collection Platform
 
-Example application for flask tutorial "[Flask 入门教程](https://helloflask.com/book/3)".
+A website used to collect annotated chemical reaction procedure data
 
-Demo: http://watchlist.helloflask.com
-
-![Screenshot](https://helloflask.com/screenshots/watchlist.png)
-
+URL: http://101.42.25.173:443
 
 ## Installation
 
-clone:
+Clone:
 ```
-$ git clone https://github.com/helloflask/watchlist.git
-$ cd watchlist
+$ git clone https://github.com/AlanZhang-2468/Data_Collection_Platform
+$ cd Data_Collection_Platform
 ```
-create & active virtual enviroment then install dependencies:
+Create & active virtual enviroment then install dependencies:
 ```
 $ python3 -m venv env  # use `python ...` on Windows
 $ source env/bin/activate  # use `env\Scripts\activate` on Windows
 (env) $ pip install -r requirements.txt
 ```
 
-generate fake data then run:
+Initialize the database
 ```
-(env) $ flask forge
+(env) $ flask initdb
+```
+Then run the website in the local laptop:
+```
 (env) $ flask run
 * Running on http://127.0.0.1:5000/
 ```
-
-
-## License
-
-This project is licensed under the MIT License (see the
-[LICENSE](LICENSE) file for details).
+If you want run the website in a certain URL, then input the following command line to run the website:
+```
+(env) $ flask run --host=your_host_address --port=your_port_address
+* Replace your_host_address and your_port_address with real address.
+```
--- app.py
+++ app.py
@@ -1,8 +1,52 @@
-from flask import render_template, request, url_for, redirect, flash
-from flask_login import login_user, login_required, logout_user, current_user
+import os
+import sys
+import click
 import numpy as np
-from data_collection_web import app, db
-from data_collection_web.models import User, Reaction
+from flask import Flask, render_template, request, url_for, redirect, flash
+from flask_sqlalchemy import SQLAlchemy
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user, current_user
+
+WIN = sys.platform.startswith('win')
+if WIN:  # 如果是 Windows 系统，使用三个斜线
+    prefix = 'sqlite:///'
+else:  # 否则使用四个斜线
+    prefix = 'sqlite:////'
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = prefix + os.path.join(app.root_path, 'data.db')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False  # 关闭对模型修改的监控
+app.config['SECRET_KEY'] = 'dev'
+# 在扩展类实例化前加载配置
+db = SQLAlchemy(app)
+login_manager = LoginManager(app)
+
+@login_manager.user_loader
+def load_user(user_id):  # 创建用户加载回调函数，接受用户 ID 作为参数
+    user = User.query.get(int(user_id))  # 用 ID 作为 User 模型的主键查询对应的用户
+    return user  # 返回用户对象
+
+class User(db.Model, UserMixin):
+    __tablename__ = 'user'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(20))
+    username = db.Column(db.String(20))  # 用户名
+    password_hash = db.Column(db.String(128))  # 密码散列值
+
+    def set_password(self, password):  # 用来设置密码的方法，接受密码作为参数
+        self.password_hash = generate_password_hash(password)  # 将生成的密码保持到对应字段
+
+    def validate_password(self, password):  # 用于验证密码的方法，接受密码作为参数
+        return check_password_hash(self.password_hash, password)  # 返回布尔值
+
+class Reaction(db.Model):
+    __tablename__ = 'reaction'
+    id = db.Column(db.Integer, primary_key=True)
+    text = db.Column(db.String(10000), nullable=False)
+    steps = db.Column(db.String(1000), nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    user = db.relationship('User', primaryjoin='Reaction.user_id == User.id')
+
 
 @app.route('/', methods=['GET', 'POST'])
 def home():
@@ -52,8 +96,8 @@ def register():
     if request.method == 'POST':
         username = request.form['username']
         password = request.form['password']
-        if len(User.query.all()) != 0:
-            user = User.query.filter_by(username=username).first()
+        # if len(User.query.all()) != 0:
+        user = User.query.filter_by(username=username).first()
         if user:
             flash('Username already exists')
             return redirect(url_for('register'))
@@ -89,6 +133,15 @@ def login():
         return redirect(url_for('login'))  # 重定向回登录页面
 
     return render_template('login.html')
+
+@app.cli.command()  # 注册为命令，可以传入 name 参数来自定义命令
+@click.option('--drop', is_flag=True, help='Create after drop.')  # 设置选项
+def initdb(drop):
+    """Initialize the database."""
+    if drop:  # 判断是否输入了选项
+        db.drop_all()
+    db.create_all()
+    click.echo('Initialized database.')  # 输出提示信息
 
 @app.route('/logout')
 @login_required  # 用于视图保护，后面会详细介绍
@@ -157,3 +210,5 @@ def delete(reaction_id):
     db.session.commit()  # 提交数据库会话
     flash('Item deleted.')
     return redirect(url_for('user_page', username=current_user.username))  # 重定向回主页
+
+
